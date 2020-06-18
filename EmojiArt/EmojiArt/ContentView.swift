a0e8537efe62ed3630d93d5c66afd7fc53342023
Ben Zobrist <ben@zobie.com>
2020-06-18 11:36:03 -0400

    Implementation for Lecture 7

---
+++ EmojiArt/EmojiArt/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Ben Zobrist on 6/18/20.
+//  Copyright © 2020 Ben Zobrist. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
---
+++ EmojiArt/EmojiArt/EmojiArt.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Ben Zobrist on 6/18/20.
+//  Copyright © 2020 Ben Zobrist. All rights reserved.
+//
+
+import Foundation
+
+struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int // offset from the center
+        var y: Int // offset from the center
+        var size: Int
+        let id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+    
+}
